088e6eb2e9466200cdc52cc357892d603126d173
Jakub Gedeon <jakub.gedeon@webfilings.com>
2014-01-24 15:18:46 -0700

    Deleted accidental spam from commit

--- src/lexer/Scanner.java
+++ src/lexer/Scanner.java
@@ -61,98 +61,97 @@ public class Scanner {
 	//where does the print statement for our tokens go?
 	
 	//Note, in the following method, all boolean methods "isBlank(next)" are there as placeholders for actual logic.
-//	private Lexeme scanNumbers(){
-//		int state = 0;
-//		for (byte next : stream){
-//			switch (state){
-//			case 0: //FSA start state
-//				if (isDigit(next)){ //if next is a digit
-//					state = 1;
-//				}
-//				else return stream.emit();
-//				break;
-//			case 1: //state 1 in FSA
-//				stream.mark(Integer_Literal);
-//				if (isDigit(next)){
-//					state = 1;
-//				}
-//				else if (next == '.'){
-//					state = 2;
-//				}
-//				else if (next == 'e' || next == 'E'){
-//					state = 4;
-//				}
-//				else return stream.emit();
-//				break;
-//			case 2: //state 2 in FSA
-//				if (isDigit(next)){
-//					state = 3;
-//				}else return stream.emit();
-//				break;
-//			case 3: //state 3 in FSA accept state for fixed literal
-//				stream.mark(Fixed_Literal);
-//				if (isDigit(next)){
-//					state = 3;
-//				}
-//				else if (next == 'e' || next == 'E'){
-//					state = 4;
-//				}
-//				else return stream.emit();
-//				break;
-//			case 4://state 4
-//				if (next == '+' || next == '-'){
-//					state = 5;
-//				}
-//				else if (isDigit(next)){ //if no + or - (empty string in FSA)
-//					state = 6;
-//				}
-//				else return stream.emit();;
-//				break;
-//			case 5: //state 5
-//				if (isDigit(next)){
-//					state = 6;
-//				}
-//				else return stream.emit();
-//				break;
-//			case 6: // state 6 in FSA / accept state for Float
-//				stream.mark(Float_Literal);
-//				if (isDigit(next)){
-//					state = 6;
-//				}
-//				else return stream.emit();
-//				break;
-//			}
-//		}
-//		
-//	}
+	private Lexeme scanNumbers(){
+		int state = 0;
+		for (byte next : stream){
+			switch (state){
+			case 0: //FSA start state
+				if (isDigit(next)){ //if next is a digit
+					state = 1;
+				}
+				else return stream.emit();
+				break;
+			case 1: //state 1 in FSA
+				stream.mark(Integer_Literal);
+				if (isDigit(next)){
+					state = 1;
+				}
+				else if (next == '.'){
+					state = 2;
+				}
+				else if (next == 'e' || next == 'E'){
+					state = 4;
+				}
+				else return stream.emit();
+				break;
+			case 2: //state 2 in FSA
+				if (isDigit(next)){
+					state = 3;
+				}else return stream.emit();
+				break;
+			case 3: //state 3 in FSA accept state for fixed literal
+				stream.mark(Fixed_Literal);
+				if (isDigit(next)){
+					state = 3;
+				}
+				else if (next == 'e' || next == 'E'){
+					state = 4;
+				}
+				else return stream.emit();
+				break;
+			case 4://state 4
+				if (next == '+' || next == '-'){
+					state = 5;
+				}
+				else if (isDigit(next)){ //if no + or - (empty string in FSA)
+					state = 6;
+				}
+				else return stream.emit();;
+				break;
+			case 5: //state 5
+				if (isDigit(next)){
+					state = 6;
+				}
+				else return stream.emit();
+				break;
+			case 6: // state 6 in FSA / accept state for Float
+				stream.mark(Float_Literal);
+				if (isDigit(next)){
+					state = 6;
+				}
+				else return stream.emit();
+				break;
+			}
+		}
+	}
 	
-//	/*-brad
-//	 * FSA implementation for String
-//	 * */
-//	//Method for string FSA
-//	private Lexeme scanString(){
-//		int state = 0;
-//		for (byte next : stream){
-//			switch (state){
-//			case 0: //FSA start state
-//				if (next == '\''){ //how do you denot apostraphy and make it work?
-//					state = 1;
-//				}
-//				else return stream.emit();
-//				break;
-//			case 1:
-//				if (next == '"'){ //im unclear as to what the definition for string says it looks like '''' 
-//					state = 1;
-//				}
-//				else if (next != '\n' && next != '\''){
-//					state = 1;
-//				}else if (next == '\''){
-//					stream.mark(String_Literal);
-//					state = 2;
-//				}else return stream.emit();
-//			}
-//		}
-//	}
+	/*-brad
+	 * FSA implementation for String
+	 * */
+	//Method for string FSA
+	private Lexeme scanString(){
+		int state = 0;
+		for (byte next : stream){
+			switch (state){
+			case 0: //FSA start state
+				if (next == '\''){ //how do you denot apostraphy and make it work?
+					state = 1;
+				}
+				else return stream.emit();
+				break;
+			case 1:
+				if (next == '"'){ //im unclear as to what the definition for string says it looks like '''' 
+					state = 1;
+				}
+				else if (next != '\n' && next != '\''){
+					state = 1;
+				}else if (next == '\''){
+					stream.mark(String_Literal);
+					state = 2;
+				}else return stream.emit();
+			}
+		}
+	}
 
 	
 	
