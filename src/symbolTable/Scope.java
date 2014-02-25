b72ff18c3d8abff3754ccb026667b07d0e916cc7
Jakub Gedeon <jakub.gedeon@webfilings.com>
2014-02-25 15:47:41 -0700

    Tweaks to mesh Scope and Typeclass

--- src/symbolTable/Scope.java
+++ src/symbolTable/Scope.java
@@ -1,14 +1,11 @@
 package symbolTable;
 
-import java.lang.reflect.Type;
 import java.util.HashMap;
-
-import lexer.Token;
 
 public class Scope {
 	private Scope parent = null;
 	private String name;
-	private HashMap<String, Type> symbolTableScope;
+	private HashMap<String, Typeclass> symbolTableScope;
 	
 	//creates a new scope with the given name and parent scope.
 	Scope(String name, Scope parent){
@@ -18,18 +15,18 @@ public class Scope {
 	
 	//method that adds an identifier to the scope.
 	//takes a Type which holds the identifiers name (string) and other attributes.
-	public void add(Type type){
+	public void add(String identName, Typeclass type){
 		// checks to see if the given identifier is already used in this scope
 		// (or a parent scope), if so, an error is thrown, else the identifier
 		// is added to the scope.
 		
-			if (lookup(type.getIdentifier() != null)) {
-				System.out.println("ERROR, the identifier " + type.getIdentifier() + " is already used in a parent scope, please change the name of the identifier!");
+		if (lookup(identName) == null) {
+			System.out.println("ERROR, the identifier " + identName + " is already used in a parent scope, please change the name of the identifier!");
 
 		} else {
 			// where type.getIdentifier() returns a string containing the name
 			// of the identifier
-			symbolTableScope.put(type.getIdentifier(), type);
+			symbolTableScope.put(identName, type);
 		}
 	}
 	
@@ -38,7 +35,7 @@ public class Scope {
 	// parent scope)
 	// if found, returns Type (of the identifier)
 	// if not found, returns null.
-	public Type lookup(String identName) {
+	public Typeclass lookup(String identName) {
 
 		// if identifier is found in current scope, return the Type of
 		// identifier.
