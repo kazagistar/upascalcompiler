92e22cb669f00c85e327265ea14f2a2f671e4ddc
Andrew Wilson <andrew.w.wilson.aww@gmail.com>
2014-03-21 17:39:53 -0600

    CodeGEN

---
+++ src/codeGen/CodeGenerator.java
@@ -0,0 +1,20 @@
+package codeGen;
+
+import symbolTable.SymbolTable;;
+
+public class CodeGenerator {
+	
+	private final SymbolTable symbolTable;
+	
+	public CodeGenerator(SymbolTable symbolTable) {
+		this.symbolTable = symbolTable;
+	}
+	
+	public void emit() {
+		System.out.println("");
+	}
+	
+	public class Either {
+		
+	}
+}
